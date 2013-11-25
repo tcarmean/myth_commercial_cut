30eb66e4fdb783c34c96686fb535a3d0255cfdcb
Tim Carmean <chong@bigmaninjapan.com>
2013-11-25 14:04:29 -0500

    Update myth_commercial_cut.py
    added some imports

--- myth_commercial_cut.py
+++ myth_commercial_cut.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 
+import os
+import sys
+import base64
+import uuid
+import subprocess
+import ConfigParser
+import StringIO
+import shutil
+try:
+        import MySQLdb
+except ImportError:
+        print('You need to install the MySQLdb python module in order to use this script')
+        exit(1)
+
 if __name__ == "__main__":
 	print "hello world!"
 
