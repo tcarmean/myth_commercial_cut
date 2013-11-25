60247bb9319e77396297e67b177c6c39f6b5080f
Tim Carmean <chong@bigmaninjapan.com>
2013-11-25 14:11:53 -0500

    Update myth_commercial_cut.py
    argument checking and usage comments

--- myth_commercial_cut.py
+++ myth_commercial_cut.py
@@ -14,6 +14,13 @@ except ImportError:
         print('You need to install the MySQLdb python module in order to use this script')
         exit(1)
 
+# How you call this script:
+# $myth_commercial_cut DIR FILE
 if __name__ == "__main__":
-	print "hello world!"
+	if (len(sys.argv) !=3):
+		print('This script only accepts 2 arguments! You passed in ' + len(sys.argv) + '!')
+		exit(1)
+	
+	filename = os.path.join(sys.argv[1], sys.argv[2])
+	
 
