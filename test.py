d4a82da1c38f23f11655f23f29e5d4ed99dadf33
Tim Carmean <tecarmea@mtu.edu>
2013-11-28 09:15:17 -0500

    refactored some tests

--- test.py
+++ test.py
@@ -7,17 +7,17 @@ class testCommercialCutJob(unittest.TestCase):
     def setUp(self):
         self.ccj = CommercialCutJob(1234, '2013-11-27 02:00:00')
 
-    def testProps(self):
-        self.assertEqual(self.ccj.width, 1280)
-        self.assertEqual(self.ccj.height, 720)
-        self.assertEqual(self.ccj.fps, 59940)
-
     def testDBCredentials(self):
         self.assertIsNotNone(self.ccj.user)
         self.assertIsNotNone(self.ccj.passwd)
         self.assertIsNotNone(self.ccj.db)
         self.assertIsNotNone(self.ccj.host)
         self.assertIsNotNone(self.ccj.port)
+
+    def testProps(self):
+        self.assertEqual(self.ccj.width, 1280)
+        self.assertEqual(self.ccj.height, 720)
+        self.assertEqual(self.ccj.fps, 59940)
 
 def main():
     unittest.main()
