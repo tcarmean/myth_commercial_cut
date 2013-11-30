1b8841d6317aaa40ee4c21a5cc773e3280127d96
Tim Carmean <tecarmea@mtu.edu>
2013-11-30 09:18:44 -0500

    adjusting slow_seek math. hopefully a bit more accurate now.

--- myth_commercial_cut.py
+++ myth_commercial_cut.py
@@ -119,10 +119,10 @@ class CommercialCutJob(object):
                 # need to get the duration
                 dur = (Decimal(self.cutlist[i+1][1]) / Decimal(self.fps) * Decimal(1000)) - Decimal(slow_seek)
                 dur = ceil(dur * 1000) / 1000.0
-                if slow_seek > 30.0:
-                    skip = slow_seek - 30.0
-                    slow_seek = 30.0
-                print('avconv -i /var/lib/mythtv/recordings/1234_20131127020000.mpg -ss %s -t %s -vcodec copy -acodec copy /tmp/cc_test/ng_cc-%d.mpg' % (str(skip),str(dur),i))
+                #if slow_seek > 30.0:
+                #    skip = slow_seek - 30.0
+                #    slow_seek = 30.0
+                print('avconv -i /var/lib/mythtv/recordings/1234_20131127020000.mpg -ss %s -t %s -vcodec copy -acodec copy /tmp/cc_test/ng_cc-%d.mpg' % (str(slow_seek),str(dur),i))
          
 
 
